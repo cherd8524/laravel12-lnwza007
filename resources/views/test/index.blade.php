1b1979b8f72c7f60eb275866543e2e3402d53fd2
BentsuKun <cherd8524@gmail.com>
2025-06-29 14:18:38 +0700

    MVC Learning

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    link
+</head>
+<body>
+    
+    <h1>เชิดศักดิ์ คำไล้ 67222420006</h1>
+    <img src="{{ $ant }}" alt="Ant Image">
+    <img src="{{ $bird }}" alt="Bird Image">
+    <img src="{{ $cat }}" alt="Cat Image">
+    <script>
+        const
+    </script>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,29 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
+Route::get( "/gallery" , function(){
+	$ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
+    $bird = "https://images.indianexpress.com/2021/03/falcon-anthony-mackie-1200.jpg"; 
+    $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
+
+    return view("test/index", compact("ant","bird","cat"));
+})->name('gallery');
+
+Route::get("/gallery/ant", function () {
+    $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
+    return view("test/ant", compact("ant"));
+});
+
+Route::get("/gallery/bird", function () {
+    $bird = "https://images.indianexpress.com/2021/03/falcon-anthony-mackie-1200.jpg";
+    return view("test/bird", compact("bird"));
+});
+
+Route::get("/gallery/cat", function () {
+    $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
+    return view("test/cat", compact("cat"));
+});
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
