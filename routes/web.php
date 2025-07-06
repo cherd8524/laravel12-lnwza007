38ee7a8de11b8d99d85df6b1f8c51f020d95a28b
BentsuKun <cherd8524@gmail.com>
2025-07-06 15:36:11 +0700

    week 3 - bootstrap theme

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+// Route::get('/', function () {
+//     return Inertia::render('welcome');
+// })->name('home');
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------------->>
 // - Week 2
@@ -32,6 +32,37 @@ Route::get("/gallery/cat", function () {
     $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
     return view("test/cat", compact("cat"));
 });
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------->>
+// - Week 3
+// ---------------------------------------------------------------------------------------------------------------------------------------------------->>
+Route::get('/index', function () {
+    return view('active/index');
+})->name('index');
+
+Route::get('/about', function () {
+    return view('active/about');
+})->name('about');
+
+Route::get('/services', function () {
+    return view('active/services');
+})->name('services');
+
+Route::get('/portfolio', function () {
+    return view('active/portfolio');
+})->name('portfolio');
+
+Route::get('/team', function () {
+    return view('active/team');
+})->name('team');
+
+Route::get('/blog', function () {
+    return view('active/blog');
+})->name('blog');
+
+Route::get('/contact', function () {
+    return view('active/contact');
+})->name('contact');
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------------->>
 
