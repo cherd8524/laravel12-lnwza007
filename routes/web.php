336868db2a655f737e40b7786495b4aaec0947b6
BentsuKun <cherd8524@gmail.com>
2025-07-20 11:58:46 +0700

    แก้ไขหน้าแรกให้เข้าถึง index

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-// Route::get('/', function () {
-//     return Inertia::render('welcome');
-// })->name('home');
+Route::get('/', function () {
+    return redirect('/index');
+});
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------------->>
 // - Week 2
