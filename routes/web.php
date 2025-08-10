0a84088b5f198146da2e561b8d0d059a5f0a5a11
BentsuKun <cherd8524@gmail.com>
2025-08-10 14:35:01 +0700

    week 6
    Migration
    - Insert
    - Query
    - API

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -36,6 +38,7 @@ Route::get("/gallery/cat", function () {
 // ---------------------------------------------------------------------------------------------------------------------------------------------------->>
 // - Week 3
 // ---------------------------------------------------------------------------------------------------------------------------------------------------->>
+
 Route::get('/index', function () {
     return view('active/index');
 })->name('index');
@@ -63,6 +66,36 @@ Route::get('/blog', function () {
 Route::get('/contact', function () {
     return view('active/contact');
 })->name('contact');
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------->>
+// - Week 6
+// ---------------------------------------------------------------------------------------------------------------------------------------------------->>
+
+Route::get('query/sql', function () {
+    $products = DB::select("SELECT * FROM products");
+    // $products = DB::select("SELECT * FROM products WHERE price > 100");
+    return view('query-test', compact('products'));
+});
+
+Route::get('query/builder', function () {
+    $products = DB::table('products')->get();
+    // $products = DB::table('products')->where('price', '>', 100)->get();
+    return view('query-test', compact('products'));
+});
+
+Route::get('query/orm', function () {
+    $products = Product::get();
+    // $products = Product::where('price', '>', 100)->get();
+    return view('query-test', compact('products'));
+});
+
+Route::get('product/form', function () {
+    //
+})->name("product.form");
+
+Route::get('barchart', function () {    
+    return view('barchart');
+})->name('barchart');
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------------->>
 
